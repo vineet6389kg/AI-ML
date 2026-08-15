8f1406b799c4780b2d1dd7e185b4ae95fb002816
vineet6389kg <vineetkumar9336a@gmail.com>
2026-08-15 19:37:35 +0530

    Is Prime Program

---
+++ 03_Loops/isPrime.py
@@ -0,0 +1,12 @@
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
